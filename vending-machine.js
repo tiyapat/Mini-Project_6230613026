90e3f0b62a8611488d61b0bf8d4c8ae1cd66d6e5
tiyapat <69616048+tiyapat@users.noreply.github.com>
2022-04-26 22:23:01 +0700

    update index

--- vending-machine.js
+++ vending-machine.js
@@ -1,24 +1,44 @@
 import Head from 'next/head'
+import Web3 from 'web3'
 import 'bulma/css/bulma.css'
-import style from '../styles/VendingMachine.module.css'
+import styles from '../styles/VendingMachine.module.css'
 
-export default function VendingMachine(){
-    return (
-        <div>
+const VendingMachine = () => {
+    let web3
+    //window.ethereum
+    const connectWalletHandler = () => {
+        if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+            window.ethereum.request({method: "eth_requestAccounts"})
+            web3  = new Web3(window.ethereum)
+        } else {
+            //meta mast not installed
+            console.log("Please install MetaMask")
+        }
+        
+    }
+    return(
+        <div className={styles.main}>
             <Head>
-                <title>VendingMachine App </title>
-                <meta name="description" content="A blockchain vending machine app" />
+                <title>VendingMachine App</title>
+                <meta name="description" content="A blockchain vending app" />
             </Head>
-            <nav className="navbar">
+            <nav classNAme="navbar mt-4 mb-4">
                 <div className="container">
                     <div className="navbar-brand">
-                        <h1> Vending Machine </h1>
+                        <h1>Vending Machine</h1>
                     </div>
                     <div className="navbar-end">
-                        <button className="button is-primary">Connect Wallet</button>
+                        <button onClick={connectWalletHandler} className="button is primary">Connect Wallet</button>
                     </div>
                 </div>
             </nav>
+            <section>
+                <div className="container">
+                    <p>placeholder text</p>
+                </div>
+            </section>
         </div>
     )
 }
+
+export default VendingMachine
